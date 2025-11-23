5c1e03b3124ab3f43f6625bf70ccf3141a312b55
Muhammad Ashfaq <ashfaqaku@gmail.com>
2025-11-23 12:10:52 +0500

    Update Oops.py

--- OPPS/Oops.py
+++ OPPS/Oops.py
@@ -8,4 +8,33 @@ print(car1.y)
 print(car1.z)   
 
 class mobile:
-    
+    a="iphone"
+    b="samsung"
+    c="oneplus" 
+mobile1=mobile()
+mobile2=mobile()
+mobile3=mobile()  
+
+class student:
+    pass
+student1=student()
+student2=student()  
+student3=student()
+student1.name="ashfaq"
+student1.age=36
+student1.rollno=301
+print(student1.name)
+print(student1.age)
+print(student1.rollno)
+
+
+class mobile:
+    def __init__(self,brand,price):
+        self.brand = brand
+        self.price = price
+    def info(self):
+        print(self.brand,self.price)
+
+mobile1=mobile("iphone",70000)
+
+mobile1.info()
